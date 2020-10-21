6a8a4fa1d9e2ff1da1105875b8a487f107d10a63
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-21 19:59:53 +1100

    menu and settings button added

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,22 @@ import './App.css';
 import './styles/styles.css'
 import { Timer } from "./components/timer";
 import { HealthTips } from "./components/healthTips";
+import { slide as Menu } from "react-burger-menu";
+import { Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <div>
+      <Menu>
+        <a id="About" className="menu-item">About</a>
+        <a id="Exercises" className="menu-item">Exercises</a>
+      </Menu>
+      <div className="settings-btn">
+      <Button>&#9881;</Button>
+      </div>
+      </div>
       <div className="timer">
         <Timer/>
         </div>

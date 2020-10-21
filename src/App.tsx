af4a11bfe94ff465838f2acf1771b689a4a8a4ca
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-21 18:52:02 +1100

    quick updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './styles/styles.css'
-import { Timer } from "./components/timer"
+import { Timer } from "./components/timer";
+import { HealthTips } from "./components/healthTips";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <div className="timer">
         <Timer/>
+        </div>
+        <div>
+        <HealthTips/>
       </div>
     </div>
   );
--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -4,7 +4,12 @@ import Countdown from "react-countdown";
 export const Timer =(props: any) => {
     return (
         <div>
+            <div>
             <Countdown date={Date.now() + 10000}/>
+            </div>
+            <div>
+                <button>Start and Stop</button>
+            </div>
         </div>
     )
 }

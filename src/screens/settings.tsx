cfa0262099d796b371315704a332931cfe7a1603
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-22 22:40:12 +1100

    settings structure init

--- src/screens/settings.tsx
+++ src/screens/settings.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 
-export const AboutScreen = (props: any) => {
+
+export const SettingsScreen = (props: any) => {
+
+    const saveSettings = () => {
+
+    }
+
     return (
-        <div></div>
+        <div>
+            <Row>
+                <Col><h3>Name</h3></Col>
+                <Col><h3>Duration</h3></Col>
+                <Col></Col>
+            </Row>
+            <Row>
+                <Col><p>Stand Desk</p> </Col>  
+            </Row>
+        </div>
     )
 }

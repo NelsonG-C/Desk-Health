75dde900567f4244044e82820ec7538bacc9128f
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-23 19:43:49 +1100

    updated screen and changed to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './styles/styles.css'
-import { Timer } from "./components/timer";
-import { HealthTips } from "./components/healthTips";
-import { slide as Menu } from "react-burger-menu";
-import { Button } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./App.css";
+import "./styles/styles.css";
+import { Home } from "./screens/home";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-      <Menu>
-        <a id="About" className="menu-item">About</a>
-        <a id="Exercises" className="menu-item">Exercises</a>
-      </Menu>
-      <div className="settings-btn">
-      <Button>&#9881;</Button>
-      </div>
-      </div>
-      <div className="timer">
-        <Timer/>
-        </div>
-        <div>
-        <HealthTips/>
-      </div>
+    <div>
+      <Home />
     </div>
   );
 }
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Timer } from "../components/timer";
+import { HealthTips } from "../components/healthTips";
+import { SettingsScreen } from "../screens/settings";
+import { slide as Menu } from "react-burger-menu";
+import { Button, Modal } from "react-bootstrap";
 
 export const Home = () => {
-  return <div>Homepage</div>;
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <div className="App">
+      <div>
+        <Menu>
+          <a id="About" className="menu-item" href="/">
+            About
+          </a>
+          <a id="Exercises" className="menu-item" href="/">
+            Exercises
+          </a>
+        </Menu>
+        <Modal show={show} onHide={handleClose}>
+          <SettingsScreen />
+        </Modal>
+        <div className="settings-btn">
+          <Button onClick={handleShow}>&#9881;</Button>
+        </div>
+      </div>
+      <div className="timer">
+        <Timer />
+      </div>
+      <div>
+        <HealthTips />
+      </div>
+    </div>
+  );
 };

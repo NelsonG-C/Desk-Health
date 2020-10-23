09f52c9e59fc029398bc215128a97b4b310b93e9
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-23 19:44:18 +1100

    columns added for settings modal

--- src/screens/settings.tsx
+++ src/screens/settings.tsx
@@ -1,23 +1,39 @@
-import React from "react";
-import { Row, Col } from "react-bootstrap";
-
+import React, { useState } from "react";
+import { Row, Col, Modal } from "react-bootstrap";
+import BootstrapSwitchButton from "bootstrap-switch-button-react";
 
 export const SettingsScreen = (props: any) => {
-
-    const saveSettings = () => {
-
-    }
-
-    return (
-        <div>
-            <Row>
-                <Col><h3>Name</h3></Col>
-                <Col><h3>Duration</h3></Col>
-                <Col></Col>
-            </Row>
-            <Row>
-                <Col><p>Stand Desk</p> </Col>  
-            </Row>
-        </div>
-    )
-}
+  const saveSettings = () => {};
+  return (
+    <div>
+      <Row>
+        <Col>
+          <h3>Name</h3>
+        </Col>
+        <Col>
+          <h3>Duration</h3>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Stand Desk</p>{" "}
+        </Col>
+        <Col>
+          <input type="text"></input>
+        </Col>
+        <Col>
+          <input id="chkSwitch" type="checkbox" data-toggle="switchbutton" />
+          <BootstrapSwitchButton
+            checked={false}
+            onlabel="Admin User"
+            onstyle="danger"
+            offlabel="Regular User"
+            offstyle="success"
+            style="w-100 mx-3"
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};

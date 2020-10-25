ae7e9c89bd63e6d74e2cfe91d2aaed6929fcef66
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-25 20:05:13 +1100

    store attempt

---
+++ src/store.ts
@@ -0,0 +1,12 @@
+const Store = require("electron-store");
+
+export const store = new Store({
+  schema: {
+    foo: {
+      type: "number",
+      maximum: 100,
+      minimum: 1,
+      default: 50,
+    },
+  },
+});

10f21de11a2d0b86c58400b281828b9d256a4718
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-27 22:24:50 +1100

    settings list added

--- src/screens/settings.tsx
+++ src/screens/settings.tsx
@@ -6,7 +6,7 @@ export const SettingsScreen = (props: any) => {
   const saveSettings = () => {};
   return (
     <div>
-      <Row>
+      <Row className="settings-titles">
         <Col>
           <h3>Name</h3>
         </Col>
@@ -23,15 +23,79 @@ export const SettingsScreen = (props: any) => {
           <input type="text"></input>
         </Col>
         <Col>
-          <input id="chkSwitch" type="checkbox" data-toggle="switchbutton" />
           <BootstrapSwitchButton
             checked={false}
-            onlabel="Admin User"
+            onlabel="On"
             onstyle="danger"
-            offlabel="Regular User"
+            offlabel="Off"
             offstyle="success"
-            style="w-100 mx-3"
+            style="w-50 mx-3"
           />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Work Break</p>
+        </Col>
+        <Col>
+          <input type="text"></input>
+        </Col>
+        <Col>
+          <BootstrapSwitchButton
+            checked={false}
+            onlabel="On"
+            onstyle="danger"
+            offlabel="Off"
+            offstyle="success"
+            style="w-50 mx-3"
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Eye Break</p>
+        </Col>
+        <Col>
+          <input type="text"></input>
+        </Col>
+        <Col>
+          <BootstrapSwitchButton
+            checked={false}
+            onlabel="On"
+            onstyle="danger"
+            offlabel="Off"
+            offstyle="success"
+            style="w-50 mx-3"
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>Work Day Settings</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3>Day Start Time</h3>
+        </Col>
+        <Col>
+          <input type="text" />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3>Day End Time</h3>
+        </Col>
+        <Col>
+          <input type="text" />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3>Main work break</h3>
+        </Col>
+        <Col>
+          <input type="text" />
         </Col>
       </Row>
     </div>
